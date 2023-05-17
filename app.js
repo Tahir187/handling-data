127af8f2d9d4e9a1f782aeddd42690dc5aaf76b4
Tahir bharchoond <bharctah@corp.contoursoftware.net>
2023-05-17 17:47:28 +0500

    Added some files for data handling

--- app.js
+++ app.js
@@ -13,4 +13,16 @@ const mySchema = new mongoose.Schema({
     age : Number
 });
 
+// check if application is successfullyb connected to mongodb server
+const db = mongoose.connection('mongodb://localhost/Company');
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', ()=>{
+    // all your database operations here
+})
+
+app.listen(8080, ()=>{
+    console.log('server is running at port 8080')
+})
+
+// creating a collection of emp and binding it with the schema mySchema
 const user = mongoose.model('emp', mySchema);
